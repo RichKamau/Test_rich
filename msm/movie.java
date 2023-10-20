e4cdcfec9b7217d21d353709dad3c70a8e3001bb
RichKamau <147367941+RichKamau@users.noreply.github.com>
2023-10-20 11:36:07 +0300

    Add files via upload

---
+++ msm/movie.java
@@ -0,0 +1,18 @@
+public class Movie {
+
+    public static void main(String[] args){
+
+        public Movie(int id, String title, int releaseYear, String genre) {
+            this.id = id;
+            this.title = title;
+            this.releaseYear = releaseYear;
+            this.genre = genre;
+        }
+
+    }
+
+
+  
+
+
+}
